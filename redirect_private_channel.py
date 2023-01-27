009a2d27ab5cd958bfa85fb851ae6458b88e57a4
Your Name <you@example.com>
2023-01-27 16:47:50 +0100

    final

--- redirect_private_channel.py
+++ redirect_private_channel.py
@@ -9,19 +9,24 @@ PRIVATE_CHAT_ID=-1001332191157
 REDIRECT_CHAT_ID=-1001786371730
 
 print("STARTED", datetime.now())
-with TelegramClient('MyApp', API_ID, API_HASH) as client:
-    with open("last_message_id.json", "r+") as f:
-        s = load(f)
-        last_id = s["id"]
-        for message in client.iter_messages(PRIVATE_CHAT_ID, offset_id=s["id"], reverse=True):
-            last_id = message.id
-            if message.message:
-                text = message.message
-                client.send_message(REDIRECT_CHAT_ID, message=text)
-            if message.photo:
-                filepath = client.download_media(message)
-                client.send_file(REDIRECT_CHAT_ID, filepath)
-                os.remove(filepath)
-    with open("last_message_id.json", "w+") as f:
-        dump({"id": last_id}, f)
-    
+client = TelegramClient('MyApp', API_ID, API_HASH)
+client.start()
+with open("last_message_id.json", "r+") as f:
+    s = load(f)
+    last_id = s["id"]
+    print("last id", last_id)
+    for message in client.iter_messages(PRIVATE_CHAT_ID, offset_id=s["id"], reverse=True):
+        print("message id", message.id)
+        last_id = message.id
+        if message.message:
+            text = message.message
+            client.send_message(REDIRECT_CHAT_ID, message=text)
+        if message.photo:
+            filepath = client.download_media(message)
+            client.send_file(REDIRECT_CHAT_ID, filepath)
+            os.remove(filepath)
+    print("last id", last_id)
+with open("last_message_id.json", "w+") as f:
+    dump({"id": last_id}, f)
+client.disconnect()
+print("DONE", datetime.now())
